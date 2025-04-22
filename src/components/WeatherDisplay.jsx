fc235f94731813911697c13fb11132a54bf5f759
Vikas <vikas.kumar.singh1198@gmail.com>
2025-04-22 19:42:55 +0530

    installed react icons for better ux

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { IoLocation } from "react-icons/io5";
+import { FaTemperatureHigh } from "react-icons/fa";
 const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
 const fetchWeather = async (lat, lon) => {
@@ -6,8 +8,9 @@ const fetchWeather = async (lat, lon) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&lat=${lat}&lon=${lon}&units=metric`
     const res = await fetch(url);
     const data = await res.json();
-  
+    console.log(data)
     return {
+      location:data.name,
       temp: data.main.temp,
       description: data.weather[0].description,
       icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
@@ -32,9 +35,11 @@ const WeatherDisplay = ({ onWeatherFetched}) => {
       if (!weather) return <p>Fetching weather...</p>;
   
       return (
-        <div className="flex items-center gap-2 text-sm text-gray-600">
-          <img src={weather.icon} alt="weather" className="w-8 h-8" />
-          <span>{weather.description}, {weather.temp}°C</span>
+        <div className="flex items-center gap-3 text-lg text-gray-600">
+          <img src={weather.icon} alt="weather" className="w-10 h-10" />
+          <span>{weather.description}</span>
+          <span className='flex items-center gap-2'><FaTemperatureHigh fill='orange'/>{weather.temp}°C</span>
+          <span className='flex items-center gap-2'><IoLocation fill='red'/>{weather.location}</span>
         </div>
       );
  };
